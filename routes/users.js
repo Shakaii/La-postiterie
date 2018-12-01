b77f815fb1a2e3ae5378a401855b7934537fda5b
Jordan <jordanjezequel@bbox.fr>
2018-12-01 10:26:00 +0100

    Some link on new package

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/about', function(req, res, next) {
+  res.send('about users');
+});
+
 module.exports = router;
