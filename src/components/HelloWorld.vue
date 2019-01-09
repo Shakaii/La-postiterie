afd8f4f315bd4baf878dc2515a6925bbd0225323
Jordan <jordanjezequel@bbox.fr>
2019-01-09 13:38:20 +0100

    Detect post it

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,16 @@
       Bienvenue sur la postiterie !
     </p>
     <input type="file" @change="previewImage($event.target.name, $event.target.files)" id="avatar" name="avatar" accept="image/png, image/jpeg">
-    <img :src="image" alt="avatar">
+    <div class="frame">
+      <div class="container">
+        <img id="postit" :src="image" alt="avatar">
+      </div>
+    </div>
+    <button @click="traite">Traiter</button>
   </div>
+  
 </template>
+<script src="../src/tracking.js"></script>
 
 <script>
 export default {
@@ -28,13 +35,39 @@ export default {
             let imageURL = URL.createObjectURL(imageFile)
             formData.append(fieldName, imageFile)
             this.image=imageURL;
-        }
+        },
+    traite: function () {
+      var img = document.getElementById('postit');
+      console.log(img)
+      var demoContainer = document.querySelector('.container');
+
+      var tracker = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+
+      tracker.on('track', function(event) {
+        event.data.forEach(function(rect) {
+          window.plot(rect.x, rect.y, rect.width, rect.height, rect.color);
+        });
+      });
+
+      tracking.track('#postit', tracker);
+      
+      window.plot = function(x, y, w, h, color) {
+        var rect = document.createElement('div');
+        document.querySelector('.container').appendChild(rect);
+        rect.classList.add('rect');
+        rect.style.border = '2px solid ' + color;
+        rect.style.width = w + 'px';
+        rect.style.height = h + 'px';
+        rect.style.left = (img.offsetLeft + x) + 'px';
+        rect.style.top = (img.offsetTop + y) + 'px';
+      };
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style >
 h3 {
   margin: 40px 0 0;
 }
@@ -49,4 +82,12 @@ li {
 a {
   color: #42b983;
 }
+
+.rect {
+    width: 80px;
+    height: 80px;
+    position: absolute;
+    left: -1000px;
+    top: -1000px;
+  }
 </style>
