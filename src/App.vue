02e26641e824e3a8e45e61b4c5137e737d7402e7
GeoffroyBERNARD <geof.b@laposte.net>
2019-01-11 11:45:20 +0100

    Refonte graphique

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://lh4.ggpht.com/er4T35JxjGnIFYL_p7HU9G0GFwNJ2eZzt1oGloPL9RC18f_MUU4h5_wwHI6IGGKWIuw=w315">
-    <Postiterie msg="Bienvenue sur La Postiterie"/>
+    <div class="header">
+      <img class="logo" src="./assets/logo.png" alt="logo">
+      <span>Bienvenue sur la postiterie</span>
+      <span class="slogan">"Gagner du temps, c'est notre slogan"</span>
+      <a class="link" target="_blank" href="http://postiterie.wordpress.com">En savoir plus</a>
+    </div>
+    <Postiterie/>
   </div>
 </template>
 
@@ -17,13 +22,68 @@ export default {
 }
 </script>
 
+
 <style>
+
+@font-face {
+  font-family: "Coiny";
+  src: url('https://fonts.googleapis.com/css?family=Coiny');
+}
+
+body{
+  
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Coiny', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+input, button, .form-submit-button{
+  font-family: 'Coiny', cursive;
+}
+
+.slogan{
+  font-style:italic;
+  color: #595959;
+}
+
+.header{
+  background-image: url("./assets/nav.jpg");
+  height: 5em;
+  display:flex;
+  justify-content: space-evenly;
+  align-items:center;
+}
+
+.logo{
+  height: 100%;
+}
+
+.link{
+  text-decoration:none;
+  font-weight: 900;
+  
+  color: #1b6ab5;
+}
+
+.link:hover{
+  color: #2080db;
+}
+
+body{
+  background-image: url("./assets/texture.jpg");
+}
+
+@media only screen and (max-width: 800px) {
+  .slogan, .link{
+    display:none;
+  }
+}
+
 </style>
--- src/components/Postiterie.vue
+++ src/components/Postiterie.vue
@@ -1,13 +1,23 @@
 <template>
+  
   <div>
-    <h1>{{ msg }}</h1>
-    <input type="file" @change="previewImage($event.target.name, $event.target.files)" accept="image/png, image/jpeg">
+    <div class="control">
+      <label  for="file" class="label-input"><span class="button">Choisir une image</span></label>
+      <input id="file" type="file" class="button-input" @change="previewImage($event.target.name, $event.target.files)" accept="image/png, image/jpeg">
+      <span id="startTracking" class="button hidden" @click="tracking">Traiter</span><br>
+    </div>
     <div class="frame">
       <div class="container">
         <img id="preview" :src="image" alt="">
       </div>
     </div>
-    <button @click="tracking">Traiter</button>
+    <div id="result-hr" class="none">
+      <div class="hr">
+        <span class="hr-img">.</span>
+        <span>Résultat</span>
+        <span class="hr-img">.</span>
+      </div>
+    </div>
     <div id="result"></div>
   </div>
   
@@ -73,11 +83,12 @@ export default {
 
     /*
       FUNCTION previewImage
-      preview the uploaded image
+      preview the uploaded image (and activate the button to track)
       IN : fieldName : the fieldName ;)
            file : the image
     */
     previewImage: function(fieldName, file) {
+            document.getElementById("startTracking").classList.remove("hidden");
             let imageFile = file[0] 
             let formData = new FormData()
             let imageURL = URL.createObjectURL(imageFile)
@@ -92,6 +103,9 @@ export default {
       Track post-its and draw a square around them
     */
     tracking: function () {
+
+      document.getElementById('result-hr').classList.remove("none");;
+
       let img = document.getElementById('preview');
       var demoContainer = document.querySelector('.container');
 
@@ -126,6 +140,54 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style >
+
+.hidden{
+  opacity: 0.5;
+}
+
+.none{
+  display:none;
+}
+
+.control{
+  display:flex;
+  justify-content: center;
+  align-items:center;
+}
+
+.button-input {
+    display: none;
+}
+
+.button{
+  margin: 1em;
+  background-image: url("../assets/nav.jpg");
+  cursor:pointer;
+  background-color: #7a9cd3; 
+  border: none;
+  color: white;
+  border-top-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1em;
+  border-style:outset;
+  border-color: #879ec4;
+  color:black;
+  box-sizing: border-box;
+}
+
+.button:hover{
+}
+
+.button:active{
+  border-color: #879ec4;
+  border-style: inset;
+}
+
+
+
 h3 {
   margin: 40px 0 0;
 }
@@ -148,4 +210,22 @@ a {
     left: -1000px;
     top: -1000px;
   }
+
+.hr{
+  display:flex;
+  flex-direction: row;
+  justify-content:space-around;
+  align-items: center;
+  margin: 2em;
+}
+
+.hr-img{
+  border-radius: 1em;
+  background-image: url("../assets/nav.jpg");
+  width: 40%;
+  height: 100%;
+  color: rgba(0,0,0,0)
+}
+
+
 </style>
