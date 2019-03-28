a80b7be58804d967a5805a2d1484a3b5856f0481
GeoffroyBERNARD <GeoffroyBERNARD>
2019-03-28 10:16:25 +0100

    bad email is correctly dealed with

--- src/components/UploadPhoto.vue
+++ src/components/UploadPhoto.vue
@@ -25,6 +25,13 @@ export default {
             isEmailValid: false
         };
     },
+    mounted(){
+        //if email was saved we check it first at creation
+        if (this.email){
+            console.log("test");
+            this.checkEmail();
+        }
+    },
     methods: {
         checkEmail(){
             let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
